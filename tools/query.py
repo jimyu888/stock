b643538e23c4533967e2f5f51297ebdd44c4f590
Jim Yu <jimyu888@gmail.com>
2019-11-30 17:29:12 -0500

    finished find with or w/o sort() or limit()

--- tools/query.py
+++ tools/query.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+'''
+This script runs mongo queries and output in an aligned structure(like MySQL) unlike the default output in Mongo shell
+
+It connects to mongodb_host(defined in your hosts file)'s stock database.
+
+Its first parameter is a file which contains a Mongo query.
+
+If not parameter specified, usuage will be printed out.
+'''
+
 import ast
 import pymongo
 import re
@@ -20,7 +30,29 @@ def parseMongoQuery(queryStr):
             limit = limit[1:len(limit)-1]
         else:
             sort = sort[1:len(sort)-1]
+    else:
+        parts = queryStr.split('.limit')
+        if len(parts)>1:
+            query = parts[0]
+	    limit = parts[1]
+            limit = limit[1:len(limit)-1]
+
     return (query, sort, limit)
+
+def getSortParams(sortStr):
+    sortParamsArray = []
+    if sortStr == '{}':
+	return sortParamsArray
+    sortStr = sortStr[1:len(sortStr)-1]
+    parts = sortStr.split(',')
+    for p in parts:
+	params = p.split(':')
+	key = params[0].strip()
+	key = key[1:len(key)-1]
+	value = params[1].strip()
+	value = int(value)
+	sortParamsArray.append((key, value))
+    return sortParamsArray
 
 client = pymongo.MongoClient("mongodb://mongodb_host:27017/")
 db = client['stock']
@@ -42,7 +74,7 @@ matches = re.match('db\.(\w+)\.(\w+)\((.*)\)(\.sort\(.*\))?(\.limit\(.*\))?', qu
 if matches:
     collectionName = matches.groups()[0]
     action = matches.groups()[1]
-    sortParams = eval(sortStr)
+    sortParams = getSortParams(sortStr)
     limitParam = limitStr
     if limitStr!='':
         limitParam = int(limitStr)
@@ -50,18 +82,21 @@ if matches:
     collection = db[collectionName]
     if action=='aggregate':
         params = ast.literal_eval(matches.groups()[2])
-        cursor = collection.aggregate(params).sort(sortParams).limit(limitParam)
+        cursor = collection.aggregate(params)
     elif action=='find':
         params = ast.literal_eval('[' + matches.groups()[2] + ']')
         cond = params[0]
         output = {}
         if len(params)>1:
             output = params[1]
-        print('DEBUG')
-        print(sortParams)
-        print(limitParam)
-        # cursor = collection.find(cond, output).sort(sortParams).limit(limitParam)
-        cursor = collection.find(cond, output).limit(limitParam)
+	if len(sortParams)>0 and limitParam:
+            cursor = collection.find(cond, output).sort(sortParams).limit(limitParam)
+        elif len(sortParams)>0:
+            cursor = collection.find(cond, output).sort(sortParams)
+        elif limitParam:
+            cursor = collection.find(cond, output).limit(limitParam)
+        else:
+            cursor = collection.find(cond, output)
 
     result = list(cursor)
     keys = []
@@ -87,5 +122,8 @@ if matches:
     for x in result:
         data = []
         for k in keys:
-            data.append(x[k])
+	    value = x[k]
+	    if value == '\\N':
+		value = ''
+            data.append(value)
         print(fmtData % tuple(data))
